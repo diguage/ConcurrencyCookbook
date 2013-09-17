e88e6df47e379af2905555050fe0c9ab4e00a5c8
D瓜哥 <leejun119@gmail.com>
2013-09-17 14:06:41 +0800

    删除多余文件，添加脚本说明

--- tools/append.rb
+++ tools/append.rb
@@ -1,3 +1,6 @@
+# Author D瓜哥，http://www.diguage.com/
+# 将模板文件添加到每个文件里
+
 
 # 扫描指定目录下到所有文件
 def scanFiles(dir) 
@@ -10,6 +13,7 @@ def scanFiles(dir)
   return list.sort
 end
 
+
 scanFiles(Dir.pwd + "/../docs/").each do |f|
 
   file = File.open(Dir.pwd + "/../docs/#{f.to_s}", "a")
@@ -19,4 +23,10 @@ scanFiles(Dir.pwd + "/../docs/").each do |f|
     file.puts line
   end
   file.close
+  template.close
 end
+
+# 获取当前路径
+# require 'pathname'
+# 
+# puts Pathname.new(File.dirname(__FILE__)).realpath
