fb8c39ade9b76414979d244052e85caa09767c4b
diguage <leejun119@gmail.com>
2013-10-05 22:52:18 +0800

    完成第四章第二节固定数目的执行器Executor示例

---
+++ src/com/diguage/books/concurrencycookbook/chapter4/recipe2/Main.java
@@ -0,0 +1,19 @@
+package com.diguage.books.concurrencycookbook.chapter4.recipe2;
+
+/**
+ * 固定数目的执行器Executor示例：主类
+ * <p/>
+ * Coder: D瓜哥，http://www.diguage.com/
+ * Date: 2013-10-05
+ * Time: 22:22
+ */
+public class Main {
+    public static void main(String[] args) {
+        Server server = new Server();
+        for (int i = 0; i < 100; i++) {
+            Task task = new Task("Task-" + i);
+            server.executeTask(task);
+        }
+        server.endServer();
+    }
+}
---
+++ src/com/diguage/books/concurrencycookbook/chapter4/recipe2/Task.java
@@ -0,0 +1,43 @@
+package com.diguage.books.concurrencycookbook.chapter4.recipe2;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 固定数目的执行器Executor示例：任务类
+ * <p/>
+ * Coder: D瓜哥，http://www.diguage.com/
+ * Date: 2013-10-05
+ * Time: 22:10
+ */
+public class Task implements Runnable {
+    private Date initDate;
+    private String name;
+
+    public Task(String name) {
+        this.initDate = new Date();
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        System.out.printf("%s: Task %s: Created on: %s\n",
+                Thread.currentThread().getName(),
+                name,
+                new Date());
+        try {
+            long duration = (long) (Math.random() * 10);
+            System.out.printf("%s: Task %s: Doing a task during %d seconds\n",
+                    Thread.currentThread().getName(),
+                    name,
+                    duration);
+            TimeUnit.SECONDS.sleep(duration);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.printf("%s: Task %s: Finished on: %s\n",
+                Thread.currentThread().getName(),
+                name,
+                new Date());
+    }
+}
